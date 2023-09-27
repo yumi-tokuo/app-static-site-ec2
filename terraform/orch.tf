34cfe527ee5f751cf1abf961cf66ffa003fae3f9
Kledson Basso <kledsonhugo@gmail.com>
2023-09-27 12:43:53 -0300

    Contract Test

--- terraform/orch.tf
+++ terraform/orch.tf
@@ -8,6 +8,6 @@ module "ec2" {
     source    = "./modules/ec2"
     rede_id   = "${module.rede.vpc_id}"
     rede_cidr = "20.0.0.0/16"
-    ami       = "lalala-ami-0f409bae3775dc8e5"
+    ami       = "ami-0f409bae3775dc8e5"
     subnet_id = "${module.rede.subnet_id}"
 }
