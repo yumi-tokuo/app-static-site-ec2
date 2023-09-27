151d571bf441637c6571968a39ef4a137cfd151c
Kledson Basso <kledsonhugo@gmail.com>
2023-09-27 12:37:57 -0300

    Contract Test

--- terraform/modules/ec2/vars.tf
+++ terraform/modules/ec2/vars.tf
@@ -5,9 +5,18 @@ variable "rede_cidr" {
     default = "10.0.0.0/16"
 }
 
+variable "port_http" {
+    type    = string
+    default = "22"
+}
+
 variable "ami" {
     type    = string
     default = "ami-02e136e904f3da870"
+    validation {
+        condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+        error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
+    }
 }
 
 variable "subnet_id" {}
