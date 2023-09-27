b30219581df9b4ab0af26af4efbb4af801b642b9
Kledson Basso <kledsonhugo@gmail.com>
2023-09-27 12:39:07 -0300

    Contract Test

--- terraform/modules/ec2/vars.tf
+++ terraform/modules/ec2/vars.tf
@@ -14,7 +14,7 @@ variable "ami" {
     type    = string
     default = "ami-02e136e904f3da870"
     validation {
-        condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+        condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
         error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
     }
 }
